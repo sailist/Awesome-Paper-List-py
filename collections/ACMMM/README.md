39e57ce51bc43be3397217cacf501472169c7499
sailist <sailist@outlook.com>
2023-02-02 12:12:12 +0800

    remove acmmm mannly saved html file

--- collections/ACMMM/README.md
+++
@@ -1,5 +0,0 @@
-
- - [2021](https://2021.acmmm.org/main-track-list), found from [hrer](https://2021.acmmm.org/)
- - [2020](https://2020.acmmm.org/main-track-list.html)
- - [ ] [2019](https://2019.acmmm.org/accepted-papers/index.html)
- - [ ] [2018](https://2018.acmmmsys.org/program/accepted-papers/)
--- scripts/crawl/acmmm.py
+++ scripts/crawl/acmmm.py
@@ -13,27 +13,6 @@ class ACMMM(Crawl):
         self.year = year
         self.link = link
 
-    def parse_2020(self):
-        res = requests.get("https://2020.acmmm.org/main-track-list.html")
-        soup = BeautifulSoup(res.content.decode(), features="lxml")
-        ps = soup.find_all('p')
-        ps = [p.get_text().strip() for p in ps]
-        ps = [re.sub('^([0-9]+) +- +', '', i) for i in ps]
-        for item in ps:
-            self.append_item('2020', item)
-
-    def parse_2021(self):
-        with open('2021.html') as r:
-            soup = BeautifulSoup(r.read(), features="lxml")
-            ps = soup.find_all('p')
-            ps = [p.get_text().strip() for p in ps]
-            ps = [
-                re.sub('^([0-9]+) *', '', i) for i in ps
-                if re.search('^([0-9]+) *', i)
-            ]
-            for item in ps:
-                self.append_item('2021', item)
-
     def parse(self):
         year = self.year
         res = requests.get(self.link)
